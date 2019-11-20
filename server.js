a011a349ca833bd3a731c848ee0610d9d1428fe9
Juan Rafael Valerio <juanra_vm@hotmail.com>
2019-11-20 00:08:14 -0600

    Heroku config v6

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import telegramRoutes from './routes/telegram.routes'
 const app = express();
 
 //Settings
-app.set('port',process.env.PORT || 80);
+app.set('port',process.env.PORT || 3000);
 
 //middleware
 app.use(cors(/*{origin: ['https://web-game-fc8f9.firebaseapp.com','http://localhost:4200']}*/));
@@ -27,3 +27,5 @@ require('./models/db.model')//execute dbconnection
 app.listen(app.get('port'), () => {
     console.log("App now running on port", app.get('port'));
   });
+
+  //"MONGODB_URI": "mongodb://localhost:27017/alexa027_bot"
